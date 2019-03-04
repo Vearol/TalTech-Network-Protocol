3cf4bc3c420b3e73942be6340b4fbc93d000a1f6
Vearol <lyubomyr.kv@gmail.com>
2019-03-04 13:00:35 +0200

    Modify server and client

--- client.py
+++ client.py
@@ -2,11 +2,23 @@
 
 import socket
 
-port = 8080
+default_server_port = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
     while True:
-        message = input('Enter message ')
-        s.sendto(bytes(message, 'utf-8'), ('127.0.0.1', port))
+        text_message = input('Enter message: ')
+
+        destination_ip = input('Enter destination ip: ')
+        destination_port = input('Enter destination port: ')
+
+        try:
+            destination_port_int = int(destination_port)
+        
+            message = text_message + '|' + destination_ip + ':' + destination_port
+
+            s.sendto(bytes(message, 'utf-8'), (destination_ip, destination_port_int))
+
+        except:
+            s.sendto(bytes(text_message, 'utf-8'), ('127.0.0.1', default_server_port))
 
--- server.py
+++ server.py
@@ -2,11 +2,12 @@
 
 import socket
 
-port = 8080
+default_server_ip = '127.0.0.1'
+default_server_port = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind(('127.0.0.1', port))
-    print('Listening socket: 127.0.0.1:', port)
+    s.bind(('127.0.0.1', default_server_port))
+    print('Listening socket: 127.0.0.1:', default_server_port)
 
     while True:
         message_bytes, address_from = s.recvfrom(10240)
@@ -18,19 +19,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         
         print("Message recieved:", message_text)
         
-        try:
+        message_destination = "127.0.0.1:8080"
+
+    try:
             message_destination = message_params[1]
             
-            print("Message destination:", message_destination)
-    
-            destination_params = message_destination.split(':')
-            destination_ip = destination_params[0]      
-            destination_port = destination_params[1]
-            
-            s.sendto(bytes(message_text, 'utf-8'), (destination_ip, int(destination_port)))
+            print("Message destination:", message_destination)            
 
-        except:
+    except:
             print('Message destination is invalid')
+
+    try:
+        destination_params = message_destination.split(':')
+
+        destination_ip = destination_params[0]
+        destination_port = destination_params[1]
+            
+        if (destination_ip == default_server_ip and destination_port == default_server_port):
+            print('Message for this server')
+        else:
+            s.sendto(bytes(message_text, 'utf-8'), (destination_ip, int(destination_port)))
+    except:
+        print('Couldn\'t parse destination params')
 
     
     
