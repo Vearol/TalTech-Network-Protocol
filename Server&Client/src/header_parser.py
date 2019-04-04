bcc93ceb6f3570f4319038d8056b69f0e6cec2e6
Vearol <lyubomyr.kv@gmail.com>
2019-04-04 19:40:41 +0300

    Add header parser

---
+++ Server&Client/src/header_parser.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+
+class Header_Parser:
+    def get_bits_value(self, number, bits):
+        value = 0
+        for i in range(bits):
+            value |= number & (1 << i)
+        
+        return value
+
+    def bytes_to_int(self, bytes):
+        result = 0
+        for b in bytes:
+            result = result * 256 + int(b)
+
+        return result
+
+    def parse_flag(self, header_bytes, bits):
+        self.flag = self.get_bits_value(header_bytes, bits)
+        
+    def parse_packet_type(self, header_bytes, bits):
+        self.packet_type = self.get_bits_value(header_bytes, bits)
+
+    def parse_protocol_version(self, header_bytes, bits):
+        self.protocol_version = self.get_bits_value(header_bytes, bits)
+    
+
+    def parse_header(self, header_bytes):
+        #byte 0        -- protocol version,packet type, flags
+        byte0 = header_bytes[0]
+
+        flag_bits = 3
+        packet_type_bits = 3
+        protocol_version_bits = 2
+
+        self.parse_flag(byte0, flag_bits)
+        byte0 = byte0 >> flag_bits
+        self.parse_packet_type(byte0, packet_type_bits)
+        byte0 = byte0 >> packet_type_bits
+        self.parse_protocol_version(byte0, protocol_version_bits)
+
+        #byte 1..8     -- SRC ID
+        byte1_8 = header_bytes[1:9]
+        self.source = self.bytes_to_int(byte1_8)
+                
+        #byte 9..16    -- DST ID
+        byte9_16 = header_bytes[9:17]
+        self.destination = self.bytes_to_int(byte9_16)
+
+        #byte 17       -- sessionID, identify flow between endpoints
+        byte17 = header_bytes[17]
+        self.session_Id = byte17
+
+        #byte 18..19   -- sequence number
+        byte18_19 = header_bytes[18:20]
+        self.sequence_number = self.bytes_to_int(byte18_19)
+
