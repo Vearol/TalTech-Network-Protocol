4ce8b8a0be358e4054dffe1cc4e7f4f1eb83714c
inar1 <inar1@protonmail.com>
2019-04-20 00:29:17 +0300

    add .gitignore

--- Server&Client/src/production/server.py
+++ Server&Client/src/production/server.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
 
-import socket
-import sys
-sys.path.append("./") 
-
 from header_parser import Header_Parser
 from packet_type_handler import handle_packet
 from flag_handler import handle_flag
 from forward import forward_message
 from sessions import UserSessions
-from message import *
+from message import UserMessageACK
+import socket
+import sys
+sys.path.append("./")
+
 
 default_server_ip = '127.0.0.1'
 default_server_port = 8080
@@ -33,8 +33,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
 
         try:
             parser.parse_header(header)
-            
-        except:
+
+        except ValueError:  # temporary set valuerror
             print('Couldn\'t parse header, skip packet')
             continue
 
@@ -42,16 +42,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         if (parser.destination != default_server_gpg):
             forward_message(sock, message_bytes, parser.destination)
             continue
-        
+
         # Payload - next 80 bytes(rest of bytes)
         payload = message_bytes[20:]
 
         sessions = UserSessions()
-        messages_ack = UserMeesageACK()
+        messages_ack = UserMessageACK()
 
-        handle_flag(sock sessions, messages_ack, parser, payload)
+        handle_flag(sock, sessions, messages_ack, parser, payload)
         handle_packet(sock, parser, payload)
-        
 
 
-#s.sendto(bytes(message_text, 'utf-8'), (destination_ip, int(destination_port)))
+# s.sendto(bytes(message_text, 'utf-8'),
+# (destination_ip, int(destination_port)))
