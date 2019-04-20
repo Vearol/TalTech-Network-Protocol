f4822fd91050d5317c50279d13219f4d3b71174d
inar1 <inar1@protonmail.com>
2019-04-20 12:00:05 +0300

    add some functions in transmission.py

--- Server&Client/src/production/transmission.py
+++ Server&Client/src/production/transmission.py
@@ -18,3 +18,36 @@ class Transmission:
     def forward_packet(sock, message, dest_ip, dest_port):
         print("Forwarding massage to {}:{}".format(dest_ip, dest_port))
         sock.sendto(message, (dest_ip, dest_port))
+
+    @staticmethod
+    def send_keepalive(sock, message, dest_ip, dest_port):
+        print("send keepalive")
+
+    @staticmethod
+    def notify_route_update(sock, message, dest_ip, dest_port):
+        print("route_update_message")
+
+    @staticmethod
+    def request_full_table(sock, message, dest_ip, dest_port):
+        print("send full table request")
+
+    @staticmethod
+    def send_full_table(sock, message, dest_ip, dest_port):
+        print("send full table data")
+
+    @staticmethod
+    def send_screen_message(sock, message, dest_ip, dest_port):
+        message_str = message.decode('utf-8')
+        print("message for screen from")
+
+    @staticmethod
+    def send_metadata(sock, message, dest_ip, dest_port):
+        print("message with binary metadata")
+
+    @staticmethod
+    def request_identity(sock, message, dest_ip, dest_port):
+        print("request identity")
+
+    @staticmethod
+    def send_identity():
+        print("send identity")
