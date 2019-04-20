efdcacbdf03948e26b951049f94f595a588db2e4
inar1 <inar1@protonmail.com>
2019-04-20 10:20:18 +0300

    put together features sending packet

---
+++ Server&Client/src/production/transmission.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+
+import socket
+
+
+TRANSMISSION_PORT = 1234
+
+
+class Transmission:
+
+    @staticmethod
+    def broadcast_update_request(sock):
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind("", TRANSMISSION_PORT)
+        # sock.sendto()
+
+    @staticmethod
+    def forward(sock, message, dest_ip, dest_port):
+        print("Forwarding massage to {}:{}".format(dest_ip, dest_port))
+        sock.sendto(message, (dest_ip, dest_port))
