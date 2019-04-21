4461c6b64934239e3808f18717a141a6ac4cb841
inar1 <inar1@protonmail.com>
2019-04-21 19:30:15 +0300

    add belman-ford and its test

--- Server&Client/src/production/test/test_nodes.py
+++ Server&Client/src/production/test/test_nodes.py
@@ -18,18 +18,24 @@ UPDATE_TABLE = {
     "A": ["C", "D"],
     "B": ["A", "E"]}
 
+
+
 node_data = Nodes()
 node_data.update_route_table(FULL_TABLE)
+node_data.add_neighbor("B", "xxx.xxx.xxx.xxx")
+node_data.add_neighbor("C", "yyy.yyy.yyy.yyy")
 
 
 def test_get_paths_num(node_data):
-    node_data.update_route_table(FULL_TABLE)
     assert 5 == node_data.get_paths_num()
 
 
-def test_get_shortest_path():
-    # Illyaaaaaaaaaaaaaa
-    pass
+def test_get_shortest_neighbor(node_data, base, target):
+    history = node_data.get_shortest_path_data("A", target)
+    test = node_data.get_shortest_neighbor(target, history)
+    assert "B" == test
 
 
 test_get_paths_num(node_data)
+# test_get_shortest_neighbor(node_data, "A", "F")
+test_get_shortest_neighbor(node_data, "A", "E")
