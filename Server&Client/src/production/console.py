e42e0703fcd5e04fa0408d64e6a6ecaeb509f71e
inar1 <inar1@protonmail.com>
2019-05-07 08:41:11 +0300

    modified the contact list command

--- Server&Client/src/production/console.py
+++ Server&Client/src/production/console.py
@@ -47,6 +47,8 @@ class Console:
                 cnt = 1
                 for key, value in nodes_data.items():
                     ip = value[0]
+                    if not ip:
+                        ip = "Non-neighbor node"
                     nickname = value[2]
                     msg = str(cnt) + ". " + nickname + ":\t" + key + "\t" + ip
                     print(self.create_msg(msg))
